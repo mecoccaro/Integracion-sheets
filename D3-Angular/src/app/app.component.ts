fe81a4f47b9745dbeef96a9e91f63b1c474544c0
mecoccaro <miguele.coccaro@yahoo.com>
2021-05-20 19:41:13 -0400

    Waterfall chart

--- D3-Angular/src/app/app.component.ts
+++ D3-Angular/src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'D3-Angular';
+  waterfalData=[
+    {
+        "name": "FY16 Value",
+        "value": 147.0,
+        "class": "total",
+        "start": 0
+    },
+    {
+        "name": "Acct Planning",
+        "value": 148.4
+    },
+    {
+        "name": "Global Accomodations",
+        "value": 151.7
+    },
+    {
+        "name": "MKT Funding",
+        "value": 151.7
+    },
+    {
+        "name": "Price",
+        "value": 151.7
+    },
+    {
+        "name": "Customer Analytics",
+        "value": 161.7
+    },
+    {
+        "name": "Digital Templates",
+        "value": 161.7
+    },
+    {
+        "name": "FY17 Value",
+        "value": 161.7,
+        "class": "total",
+        "start": 0
+    }
+];
 }
